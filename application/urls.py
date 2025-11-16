004a04c5cf2ee372024a6c43d2ecb88936a3e72e
Rose Scott <joshuascott7408@gmail.com>
2025-11-16 00:09:44 -0500

    hundreds of lines of code, all front end it kinda pretty tho, fake backend at the moment temporarly

--- application/urls.py
+++ application/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
+    path('card_control/', views.card_control, name='card_control'),
+    path('card_details/', views.card_details, name='card_details'),
+    path('transaction_history/', views.transaction_history, name='transaction_history'),
+    path('fraud/', views.fraud, name='fraud'),
 ]
